41702cf78fc20d9e5d7cab4d4b65d5a9c11d1b6f
Olivier <oesukam@gmail.com>
2019-10-16 02:02:41 +0200

    remove unncessary comments

--- src/pages/SinglePost/SinglePost.jsx
+++ src/pages/SinglePost/SinglePost.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import './Home.scss';
 import Layout from '../../containers/Layout/Layout';
 import Post from '../../components/SinglePost/SinglePost';
 
