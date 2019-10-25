336148f49b0cd3880d27f8de2562066ecc05acd0
Grace <kivubox@gmail.com>
2019-10-25 10:22:42 +0200

    Add comsmetic to Home left menu

---
+++ src/constants/categories.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    name: 'All',
+    value: '',
+    icon: 'list',
+  },
+  {
+    name: 'General',
+    value: 'general',
+    icon: 'globe',
+  },
+  {
+    name: 'Jobs',
+    value: 'job',
+    icon: 'suitcase',
+  },
+  {
+    name: 'Events',
+    value: 'event',
+    icon: 'calendar',
+  },
+  {
+    name: 'Announcements',
+    value: 'announcement',
+    icon: 'bullhorn',
+  },
+  {
+    name: 'Questions',
+    value: 'question',
+    icon: 'question',
+  },
+];
