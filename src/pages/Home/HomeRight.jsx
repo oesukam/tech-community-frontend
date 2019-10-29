7312db77e3f9f6aa6bac07f95504175a6b38568e
Grace <kivubox@gmail.com>
2019-10-29 13:03:38 +0200

    Add new users on the right side of the homepage

--- src/pages/Home/HomeRight.jsx
+++ src/pages/Home/HomeRight.jsx
@@ -4,12 +4,13 @@ import { connect } from 'react-redux';
 import './HomeRight.scss';
 import { Link } from 'react-router-dom';
 import orgDefaultLogo from '../../assets/svg/organization.svg';
+import LatestUsers from '../../components/Users/Users';
 
 export class HomeRight extends Component {
   renderOrganizations = () => {
     const { organizations } = this.props;
     return organizations.map((org) => (
-      <Link key={org.slug || org._id} to={org.slug || org._id} className="org-card">
+      <Link key={org.slug || org._id} to={org.slug || org._id} className="org-card hide">
         <img src={org.image || orgDefaultLogo} className="org-card__image" alt="Logo" />
         <div className="org-card__content">
           <h1 className="org-card__content__name">{org.name}</h1>
@@ -19,17 +20,23 @@ export class HomeRight extends Component {
     ));
   }
 
+  OrganizationsList = ({ organizations }) => (
+    <>
+      {this.renderOrganizations()}
+      { organizations.length ? (
+        <Link to="/organizations" className="side-see-more">
+          See more
+        </Link>
+      ) : null}
+    </>
+  );
+
   render() {
-    const { organizations } = this.props;
+    const { OrganizationsList, props: { organizations, history } } = this;
     return (
       <div className="side-container">
-        <div className="side-container__header">Organizations</div>
-        {this.renderOrganizations()}
-        { organizations.length ? (
-          <Link to="/organizations" className="side-see-more">
-          See more
-          </Link>
-        ) : null}
+        <OrganizationsList organizations={organizations} />
+        <LatestUsers history={history} />
       </div>
     );
   }
@@ -37,6 +44,7 @@ export class HomeRight extends Component {
 
 HomeRight.propTypes = {
   organizations: PropTypes.array,
+  history: PropTypes.object.isRequired,
 };
 
 HomeRight.defaultProps = {
