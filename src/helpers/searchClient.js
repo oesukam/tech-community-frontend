b41e15ff700e5c9403ccbde455d27502538e2fd9
Espoir Murhabazi <espoir.mur@gmail.com>
2020-05-10 20:23:46 +0200

    (feat) remove search api keys

--- src/helpers/searchClient.js
+++ src/helpers/searchClient.js
@@ -1,9 +1,9 @@
 import algoliasearch from 'algoliasearch/lite';
 
-// TODO: to be remove soon
 const searchClient = algoliasearch(
-  '02MRSL032D',
-  '8465d608c043168e7f5a759aa3420767',
+  process.env.REACT_APP_ALGOLIA_APP_ID,
+  process.env.REACT_APP_ALGOLIA_APP_KEY,
+
 );
 
 export default searchClient;
